1e6c6452d7dbb9fededef326c9a76fabf109abee
unknown <shrutipethani.royal@gmail.com>
2023-04-04 13:36:32 +0530

    first

---
+++ TypeCasting.py
@@ -0,0 +1,32 @@
+'''
+int()
+bool()
+str()
+float()
+complex()
+
+'''
+
+a=34
+print(a,type(a))
+
+b = str(a)
+print(b,type(b))
+
+# str1="ayaan"
+# c=int(str1) invalid
+# print(c,type(c))
+
+s1=3452346
+s2=float(s1)
+print(s2,type(s2))
+
+s3=354.34543
+s4=int(s3)
+print(s4,type(s4))
+
+s6=0
+s5=bool(s6)
+print(s5,type(s5))
+
+# 0- False anything else - True
---
+++ first.py
@@ -0,0 +1,82 @@
+# single line comment
+
+'''
+multi
+line 
+comment
+
+'''
+
+"""
+Multi
+comment
+
+"""
+
+print("Good Morning!!")
+print('Ayaan')
+
+# My Name is Ayaan's 
+
+print("My Name is Ayaan's ")
+print('My Name is Ayaan\'s ')
+
+'''
+\'
+\"
+
+\n
+\t
+
+'''
+# print("Ayaan \nSheth")
+# print("Ayaan \t Sheth")
+
+
+print('Hello',end=" I am ")
+print('Ayaan')
+
+'''
+Types:
+
+int 
+float
+bool
+str
+
+
+'''
+
+a=34
+print(a)
+print(type(a))
+
+a=-324657654321345678965432123456789765432123456789087654323456789087654321314567890876543212345678908765
+
+print(a , type(a))
+
+b = 3.5676
+b = 6547.575487436856
+print(b,type(b))
+
+c = False
+c  =  True
+print(c , type(c))
+
+
+d="Ayaan"
+d='e'
+print(d , type(d))
+
+
+e = 4+5j
+print(e,type(e))
+print(e.real)
+print(e.imag)
+
+'''
+complex: real + imag
+real:4
+imag: 5
+
+'''
